6069bf18b580823af3e66ec86acda193febb11d2
DomEnota-hub <domenota6212@gmail.com>
2026-09-16 23:52:49 +0300

    Bump app to 1.2.2-dev11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "ru.railbrake.calculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 138
-        versionName = "1.2.2-dev10"
+        versionCode = 139
+        versionName = "1.2.2-dev11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
